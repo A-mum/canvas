545be4ee40850af2a2a471c20bba25ec3f26c2b6
A-mum <78169290+A-mum@users.noreply.github.com>
2021-03-23 13:28:51 -0400

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,9 @@
+var last_position_of_x, last_position_of_y;
+
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext ("2d"); 
+
+var MouseEvent = "empty";
+color = "auqa";
+
+width_of_line = 1
